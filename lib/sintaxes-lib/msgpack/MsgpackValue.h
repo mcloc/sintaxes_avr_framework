9db34b7928d782d506b7e7c78ad34c8fe20573ed
Mario Caseiro <mcloc@katharine.sintaxes,com.br>
2019-10-12 21:00:39 -0300

    changed concept of MsgpackValue

--- lib/sintaxes-lib/msgpack/MsgpackValue.h
+++ lib/sintaxes-lib/msgpack/MsgpackValue.h
@@ -14,6 +14,7 @@
 class MsgpackValue {
 public:
 	MsgpackValue();
+	virtual ~MsgpackValue();
 	virtual uint8_t getValueUINT8() = 0;
 	virtual uint8_t getValueUINT16() = 0;
 	virtual uint8_t getValueUINT32() = 0;
